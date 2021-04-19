d1c70c883dbd13ecdb12c7039a97c5174aa22b0d
Kid-Guru <birkov.alexandr@gmail.com>
2021-04-19 11:30:53 +0400

    add propTypes for Button

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,7 +1,16 @@
+import PropTypes from 'prop-types';
 import s from './Button.module.scss';
 
-const Button = ({ text = 'Кнопка' }) => (
+const Button = ({ text }) => (
   <button className={s.button} type="button">{text}</button>
 );
+
+Button.propTypes = {
+  text: PropTypes.string,
+};
+
+Button.defaultProps = {
+  text: 'текст',
+};
 
 export default Button;
