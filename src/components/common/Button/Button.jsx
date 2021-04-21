4b3b0ed2280d457dc220f7d48ac8fe2f583484b4
Kid-Guru <birkov.alexandr@gmail.com>
2021-04-21 11:28:13 +0400

    add callback for redirect

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,16 +1,18 @@
 import PropTypes from 'prop-types';
 import s from './Button.module.scss';
 
-const Button = ({ text }) => (
-  <button className={s.button} type="button">{text}</button>
+const Button = ({ text, onClickHandle }) => (
+  <button className={s.button} type="button" onClick={onClickHandle}>{text}</button>
 );
 
 Button.propTypes = {
   text: PropTypes.string,
+  onClickHandle: PropTypes.func,
 };
 
 Button.defaultProps = {
   text: 'текст',
+  onClickHandle: () => {},
 };
 
 export default Button;
