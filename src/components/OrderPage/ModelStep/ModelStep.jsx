8f49ac0d51e6959835ab1d3d72121ed3a1937b90
Kid-Guru <birkov.alexandr@gmail.com>
2021-04-23 15:03:20 +0400

    implemented radioBtn layout

--- src/components/OrderPage/ModelStep/ModelStep.jsx
+++ src/components/OrderPage/ModelStep/ModelStep.jsx
@@ -1,9 +1,60 @@
 import './ModelStep.scss';
 
-const ModelStep = () => (
-  <div className="modelStep">
-    Model Step
-  </div>
-);
+const ModelStep = (props) => {
+  const { getCarsRequest } = props;
+  useEffect(() => {
+    getCarsRequest();
+  }, [getCarsRequest]);
+  return (
+    <div className="modelStep">
+      <div className="modelStep__filter">
+        <label className="radioInput" htmlFor="all">
+          <input
+            className="radioInput__input"
+            id="all"
+            type="radio"
+            name="modelFilter"
+          // value="allModeles"
+          // checked={formikModels.values.modelFilter === 'allModeles'}
+          // onChange={formikModels.handleChange}
+          />
+          <span className="radioInput__text">Все модели</span>
+        </label>
+
+        <label className="radioInput" htmlFor="cheap">
+          <input
+            className="radioInput__input"
+            id="cheap"
+            type="radio"
+            name="modelFilter"
+          // value="allModeles"
+          // checked={formikModels.values.modelFilter === 'allModeles'}
+          // onChange={formikModels.handleChange}
+          />
+          <span className="radioInput__text">Эконом</span>
+        </label>
+        <label className="radioInput" htmlFor="premium">
+          <input
+            className="radioInput__input"
+            id="premium"
+            type="radio"
+            name="modelFilter"
+          // value="allModeles"
+          // checked={formikModels.values.modelFilter === 'allModeles'}
+          // onChange={formikModels.handleChange}
+          />
+          <span className="radioInput__text">Премиум</span>
+        </label>
+      </div>
+      <div className="modelStep__cars">
+        <div className="carCard">
+          <h5 className="carCard__title">ELANTRA</h5>
+          <p className="carCard__price">10 000 - 32 000 ₽</p>
+          <img src="" alt="" className="card__pic" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ModelStep;
