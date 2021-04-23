9c540611b01dfdf31bfd086dd9d3b6c1aa0594b1
Kid-Guru <birkov.alexandr@gmail.com>
2021-04-23 13:52:24 +0400

    layout implementation total component

--- src/components/OrderPage/Total/Total.jsx
+++ src/components/OrderPage/Total/Total.jsx
@@ -1,26 +1,49 @@
+import { connect } from 'react-redux';
 import Button from '../../common/Button/Button';
+import * as actions from '../../../redux/actions';
+import { isNextStepAvailable } from '../../../redux/selectors';
 import './Total.scss';
 
-const Total = () => (
-  <div className="total">
-    <h4 className="total__title">Ваш заказ</h4>
-    <div className="total__order">
-      <p className="total__item">
-        <span className="total__item-title">Пункт выдачи</span>
-        <span className="total__item-value">Ульяновск, Нариманова 42</span>
-      </p>
-      <p className="total__item">
-        <span className="total__item-title">Пункт выдачи</span>
-        <span className="total__item-value">Ульяновск, Нариманова 42</span>
-      </p>
-      <p className="total__item">
-        <span className="total__item-title">Длительность аренды</span>
-        <span className="total__item-value">Ульяновск, Нариманова 42</span>
-      </p>
-      <div className="total__price">Цена</div>
-      <Button text="Слеудющий шаг" />
+const Total = (props) => {
+  const { currentStepOrder, isNextStepAvailable, setNewStepOrder } = props;
+  return (
+    <div className="total">
+      <h4 className="total__title">Ваш заказ</h4>
+      <div className="total__order">
+        <p className="total__item">
+          <span className="total__item-title">Пункт выдачи</span>
+          <span className="total__item-value">Ульяновск, Нариманова 42</span>
+        </p>
+        <p className="total__item">
+          <span className="total__item-title">Пункт выдачи</span>
+          <span className="total__item-value">Ульяновск, Нариманова 42</span>
+        </p>
+        <p className="total__item">
+          <span className="total__item-title">Длительность аренды</span>
+          <span className="total__item-value">Ульяновск, Нариманова 42</span>
+        </p>
+      </div>
+      <div className="total__price">
+        <span className="total__price-title">Цена: </span>
+        <span className="total__price-number">10 000 ₽</span>
+      </div>
+      <Button
+        text="Слеудющий шаг"
+        isFullWidth
+        isDisabled={!isNextStepAvailable}
+        onClickHandle={() => setNewStepOrder(currentStepOrder + 1)}
+      />
     </div>
-  </div>
-);
+  );
+};
 
-export default Total;
+const mapStateToProps = (state) => ({
+  currentStepOrder: state.order.steps.currentStep,
+  isNextStepAvailable: isNextStepAvailable(state),
+});
+
+const actionCreators = ({
+  setNewStepOrder: actions.handleCurrentStepOrder,
+});
+
+export default connect(mapStateToProps, actionCreators)(Total);
