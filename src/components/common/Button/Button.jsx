ba0402681ca548e45e66c29c3389a80baa183562
Kid-Guru <birkov.alexandr@gmail.com>
2021-04-23 13:48:37 +0400

    add fullWidth prop

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -1,18 +1,35 @@
+import cn from 'classnames';
 import PropTypes from 'prop-types';
 import s from './Button.module.scss';
 
-const Button = ({ text, onClickHandle }) => (
-  <button className={s.button} type="button" onClick={onClickHandle}>{text}</button>
-);
+const Button = ({
+  text, onClickHandle, isDisabled, isFullWidth,
+}) => {
+  const buttonClass = cn(s.button, { [s.button__fullWidth]: isFullWidth });
+  return (
+    <button
+      className={buttonClass}
+      disabled={isDisabled}
+      type="button"
+      onClick={onClickHandle}
+    >
+      {text}
+    </button>
+  );
+};
 
 Button.propTypes = {
   text: PropTypes.string,
   onClickHandle: PropTypes.func,
+  isDisabled: PropTypes.bool,
+  isFullWidth: PropTypes.bool,
 };
 
 Button.defaultProps = {
   text: 'текст',
-  onClickHandle: () => {},
+  onClickHandle: () => { },
+  isDisabled: false,
+  isFullWidth: false,
 };
 
 export default Button;
