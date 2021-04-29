336e023813c6c7939a10ea0cbf15f78be21c542d
Kid-Guru <birkov.alexandr@gmail.com>
2021-04-29 10:36:14 +0400

    delete proxy, fix cors

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-const PROXYURL = 'https://cors-anywhere.herokuapp.com/';
-const APIURL = 'http://api-factory.simbirsoft1.com/api/';
+const APIURL = 'https://api-factory.simbirsoft1.com/api/';
 
 const client = axios.create({
-  baseURL: `${PROXYURL}${APIURL}`,
+  baseURL: APIURL,
   headers: {
     'X-Api-Factory-Application-Id': '5e25c641099b810b946c5d5b',
     // Authorization: '4cbcea96de',
