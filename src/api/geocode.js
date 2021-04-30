fea736c2a519282374e52ce31f59206071feb723
Kid-Guru <birkov.alexandr@gmail.com>
2021-04-30 15:11:01 +0400

    delete unused code from geocode.js

--- src/api/geocode.js
+++ src/api/geocode.js
@@ -1,15 +1,2 @@
-// import axios from 'axios';
-
 const YANDEX_MAP_API = '2106c918-2fec-450a-b18f-d2a580f03f17';
 export default YANDEX_MAP_API;
-
-// const client = axios.create({
-//   baseURL: `https://geocode-maps.yandex.ru/1.x/?apikey=${GEO_KEY}&format=json&geocode=`,
-// });
-
-// const getCoord = (city, adress = '') => {
-//   const pointAdress = city.replace(/ /g, '+') + adress.replace(/ /g, '+');
-//   return client.get(pointAdress);
-// };
-
-// export default getCoord;
