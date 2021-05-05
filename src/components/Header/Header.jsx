583a880a88525e1281a3ee217ad30c25f94f413a
Kid-Guru <birkov.alexandr@gmail.com>
2021-05-05 16:27:09 +0400

    add displaying selected city

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,24 @@
+import { useSelector } from 'react-redux';
+import cn from 'classnames';
 import BurgerButton from '../common/BurgerButton/BurgerButton';
 import './Header.scss';
 
-const Header = () => (
-  <div className="header">
-    <div className="header__btnCol">
-      <BurgerButton theme="dark" />
-    </div>
-    <div className="header__textCol">
-      <div className="header__logo">Need for drive</div>
-      <div className="s.header__city city">
-        <span className="city__name">Ульяновск</span>
+const Header = () => {
+  const { value, isValid } = useSelector((state) => state.order.cityOrder);
+  const cityClass = cn('city', { city_isHidden: !isValid });
+  return (
+    <div className="header">
+      <div className="header__btnCol">
+        <BurgerButton theme="dark" />
+      </div>
+      <div className="header__textCol">
+        <div className="header__logo">Need for drive</div>
+        <div className={`s.header__city ${cityClass}`}>
+          <span className="city__name">{value}</span>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
