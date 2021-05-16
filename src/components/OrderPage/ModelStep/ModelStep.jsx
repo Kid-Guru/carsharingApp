edcfe9530c3663f187ef14c8e173a2db22d8b700
Kid-Guru <birkov.alexandr@gmail.com>
2021-05-16 16:51:04 +0400

    add scroll  cars

--- src/components/OrderPage/ModelStep/ModelStep.jsx
+++ src/components/OrderPage/ModelStep/ModelStep.jsx
@@ -45,20 +45,22 @@ const ModelStep = () => {
         ))}
       </div>
       <div className="modelStep__cars cars">
-        <ul className="cars__list">
-          {cars.map((car) => (
-            <li
-              className={cn('cars__card', { cars__card_selected: car.id === selectedCar.id })}
-              key={car.id}
-              onClick={() => selectCar(car.id)}
-              role="presentation"
-            >
-              <h5 className="cars__cardTitle">{car.model}</h5>
-              <p className="cars__cardPrice">{car.price}</p>
-              <div className="cars__cardPic" style={{ backgroundImage: `url(${car.picPath})` }} />
-            </li>
-          ))}
-        </ul>
+        <div className="scroll">
+          <ul className="cars__list">
+            {cars.map((car) => (
+              <li
+                className={cn('cars__card', { cars__card_selected: car.id === selectedCar.id })}
+                key={car.id}
+                onClick={() => selectCar(car.id)}
+                role="presentation"
+              >
+                <h5 className="cars__cardTitle">{car.model}</h5>
+                <p className="cars__cardPrice">{car.price}</p>
+                <div className="cars__cardPic" style={{ backgroundImage: `url(${car.picPath})` }} />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
