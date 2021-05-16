017be0f0f627b648605e65021d83898b6f5fe411
Kid-Guru <birkov.alexandr@gmail.com>
2021-05-16 16:09:38 +0400

    delete icon from cancel btn

--- src/components/StatusOrderPage/CancelOrderBtn/CancelOrderBtn.jsx
+++ src/components/StatusOrderPage/CancelOrderBtn/CancelOrderBtn.jsx
@@ -1,6 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
 import FloatBtn from '../../common/FloatBtn/FloatBtn';
-import { ReactComponent as NextIcon } from '../../../assets/next-arrow.svg';
 import { getOrderData } from '../../../redux/selectors';
 import { cancelViewedOrder } from '../../../redux/viewOrder/actions';
 
@@ -14,9 +13,7 @@ const CancelOrderBtn = () => {
       text="Отменить"
       onClickHandle={() => dispatch(cancelViewedOrder())}
       red
-    >
-      <NextIcon />
-    </FloatBtn>
+    />
   );
 };
 
